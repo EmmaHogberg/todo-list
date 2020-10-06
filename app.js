c4095b8f6dd6c6807b105624e0fae745be2ace73
EmmaHogberg <70634183+EmmaHogberg@users.noreply.github.com>
2020-10-06 12:43:58 +0200

    Start av Todo list

---
+++ app.js
@@ -0,0 +1,6 @@
+const addTodoForm = document.querySelector("#addTodoForm");
+console.log(addTodoForm);
+
+const addTodoInput = document.querySelector("#addTodoInputField");
+
+const addToList = document.querySelector("#listItem");
