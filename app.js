a32293454b97d90feb6f7a3add38d83dd267fd07
EmmaHogberg <70634183+EmmaHogberg@users.noreply.github.com>
2020-10-06 15:14:11 +0200

    First release

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const addTodoForm = document.querySelector("#addTodoForm");
-console.log(addTodoForm);
 
 const addTodoInput = document.querySelector("#addTodoInputField");
 
-const addToList = document.querySelector("#listItem");
+const todoList = document.querySelector("#todoList");
+
+// Set focus on text input
+addTodoInput.focus();
+
+// Add event listener for submitting form
+addTodoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTodo(addTodoInput.value);
+});
+
+function addTodo(todoText) {
+  todoList.insertAdjacentHTML(
+    "beforeend",
+    `<li>${todoText}<button onclick="deleteTodo(this)">delete</button></li>`
+  );
+  addTodoForm.reset();
+}
+function deleteTodo(element) {
+  element.parentElement.remove();
+  addTodoInput.focus();
+}
